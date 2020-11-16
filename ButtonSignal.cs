8a4b6663610660407cd938680ec3c1a75656f48b
fdayet <72388930+fdayet@users.noreply.github.com>
2020-11-16 08:38:22 +0100

    Add button scripts

---
+++ ButtonSignal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSignal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        transform.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+    }
+    
+    public void enableButton()
+    {
+        GetComponent<MeshRenderer>().enabled = true;
+        GetComponent<Rigidbody>().isKinematic = false;
+        transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+    }
+}
