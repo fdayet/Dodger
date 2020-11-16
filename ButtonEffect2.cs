034aec1fd1bcd577882637b499050f2083435b36
fdayet <72388930+fdayet@users.noreply.github.com>
2020-11-16 08:37:07 +0100

    Add files via upload

---
+++ ButtonEffect2.cs
@@ -0,0 +1,25 @@
+﻿//======= Copyright (c) Valve Corporation, All rights reserved. ===============
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace Valve.VR.InteractionSystem.Sample
+{
+    public class ButtonEffect2 : MonoBehaviour
+    {
+        public AudioClip sound;
+        public void OnButtonDown(Hand fromHand)
+        {
+            GetComponent<AudioSource>().PlayOneShot(sound);
+            fromHand.TriggerHapticPulse(1000);
+        }
+
+        public void OnButtonUp(Hand fromHand)
+        {
+
+        }
+
+    }
+}
---
+++ CollisionSound.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour
+{
+    public AudioClip sound;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Desert")
+        {
+            GetComponent<AudioSource>().PlayOneShot(sound);
+        }
+    }
+}
